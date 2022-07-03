0cfa178a63f2ed1ef4fcb3bf63f1f8d3f5c5fba3
Cristhofer Magno <cristhofer.mendonca@gmail.com>
2022-07-03 01:31:13 -0300

    Improvements

--- src/types.ts
+++ src/types.ts
@@ -1,103 +1,93 @@
-import { IMessageSchema } from "./services/message/types";
+import {IMessageSchema} from './services/message/types';
 
-export type TMessageType = 
-   | "text" 
-   | "audio" 
-   | "document" 
-   | "image" 
-   | "video" 
-   | "figure" 
-   |  "microphone";
-export type TMessageDelivery = 
-    | "send" 
-    | "delivered";
+export type TMessageType =
+  | 'text'
+  | 'audio'
+  | 'document'
+  | 'image'
+  | 'video'
+  | 'figure'
+  | 'microphone';
+
+export type TMessageDelivery = 'send' | 'delivered';
+
 export interface IUser {
-    _id: string;
-    fullName: string;
-    email: string;
-    avatar: string;
-    phone: string;
-    token: string;
-    online?: boolean;
-    background: {
-        home: String;
-        people: String;
-    }
-};
+  _id: string;
+  fullName: string;
+  email: string;
+  avatar: string;
+  phone: string;
+  token: string;
+  online?: boolean;
+  background: {
+    home: String;
+    people: String;
+  };
+}
+
 export interface IUserCreate {
-    fullName: string;
-    email: string;
-    phone: string;
-    password: string;
-};
+  fullName: string;
+  email: string;
+  phone: string;
+  password: string;
+}
 
-export type TRoutesAuth = 
-    | "SplashInit"
-    | "SignIn" 
-    | "SignUp" 
-    | "VerifyCode";
+export type TRoutesAuth = 'SplashInit' | 'SignIn' | 'SignUp' | 'VerifyCode';
 
-export type TRoutesApp = 
-    | "App"
-    | "Home" 
-    | "Profile";
+export type TRoutesApp = 'App' | 'Home' | 'Profile';
 
-export type TRoutesConversation = 
-    | "Conversation";
-    
+export type TRoutesConversation = 'Conversation';
+
 export type TRouteRedirect = TRoutesAuth | TRoutesApp | TRoutesConversation;
 
 export type TStepApp = TRoutesAuth | TRoutesApp | TRoutesConversation;
 
-
 export interface RenderItem<T> {
-    item: T;
-    index: number; 
-};
+  item: T;
+  index: number;
+}
 
 export interface IPeopleItem {
-    _id: string;
-    fullName: string;
-    email: string;
-    avatar: string;
-    online: boolean;
-    active?: boolean;
-};
+  _id: string;
+  fullName: string;
+  email: string;
+  avatar: string;
+  online: boolean;
+  active?: boolean;
+}
 
 export interface IActionChat {
-    action: IUserMakingActionOnChat["action"];
-    userId: string; 
+  action: IUserMakingActionOnChat['action'];
+  userId: string;
 }
 
 export interface IChatItem {
-    _id: string;
-    creator: string;
-    createdAt: string | Date;
-    updatedAt: string | Date;
-    users: IUserChat[];
-    admin: string[];
-    lastMessage: IMessageSchema;
-    actionChat: IActionChat;
-};
+  _id: string;
+  creator: string;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  users: IUserChat[];
+  admin: string[];
+  lastMessage: IMessageSchema;
+  actionChat: IActionChat;
+}
 
-export type TCode = 
-    | "VERIFY_CODE" 
-    | "CHANGE_EMAIL";
+export type TCode = 'VERIFY_CODE' | 'CHANGE_EMAIL';
 
 export interface IUserMakingActionOnChat {
-    userId: string;
-    socketId: string;
-    chatId: string;
-    isMakingAction: boolean;
-    action: "text" | "audio" | "video" | "" | null;
-};
+  userId: string;
+  socketId: string;
+  chatId: string;
+  isMakingAction: boolean;
+  action: 'text' | 'audio' | 'video' | '' | null;
+}
 
 export interface IUserChat extends IPeopleItem {
-    action?: IUserMakingActionOnChat["action"];
+  action?: IUserMakingActionOnChat['action'];
 }
 
 export interface ISetIdUserOnSeenMessages {
-    userId: string;
-    chatId: string;
-    messagesIds: string[];
+  userId: string;
+  chatId: string;
+  messagesIds: string[];
 }
