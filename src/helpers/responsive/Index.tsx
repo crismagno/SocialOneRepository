146276829ddd385645ed2e7f5b16a0abbf2af899
Cristhofer Magno <cristhofer.mendonca@gmail.com>
2022-09-17 22:26:24 -0300

    Add improvements

--- src/helpers/responsive/Index.tsx
+++ src/helpers/responsive/Index.tsx
@@ -1,27 +1,26 @@
 /**
  * Note.: Sizes based on Phone => Pixel_4_XL_API_30 -> 1440 X 3040
  */
-import { Dimensions, Platform, PixelRatio } from "react-native";
+import {Dimensions, Platform, PixelRatio} from 'react-native';
 
-const { height: HEIGHT, width: WIDTH } = Dimensions.get("window");
+const {height: HEIGHT, width: WIDTH} = Dimensions.get('window');
 
-const SIZE_100: number = HEIGHT/8; // based in 100px
+const SIZE_100: number = HEIGHT / 8; // based in 100px
 
 /**
  * calculation for size responsiveness
-*/
-export const setSize = (size: number): number => (SIZE_100 * size)/100;
+ */
+export const setSize = (size: number): number => (SIZE_100 * size) / 100;
 
-export const getPixelSize = pixels => {
-    return Platform.select({
-        ios: pixels,
-        android: PixelRatio.getPixelSizeForLayoutSize(pixels)
-    });
-}; 
-
+export const getPixelSize = (pixels) => {
+  return Platform.select({
+    ios: pixels,
+    android: PixelRatio.getPixelSizeForLayoutSize(pixels),
+  });
+};
 
 export default {
-    SIZE_100,
-    setSize,
-    getPixelSize
+  SIZE_100,
+  setSize,
+  getPixelSize,
 };
--- src/services/user/index.ts
+++ src/services/user/index.ts
@@ -1,289 +1,290 @@
-import { IPeopleItem, IUser, IUserCreate } from "../../types";
-import request from "./../../helpers/request";
-import { IUserSignIn } from "./types";
+import {IPeopleItem, IUser, IUserCreate} from '../../types';
+import request from './../../helpers/request';
+import {IUserSignIn} from './types';
 
-export const signIn = async (user: IUserSignIn): Promise<IUser|never> => {
-    try {
-        const response = await request
-            .requestApi({
-                route: `user/signin`,
-                body: user,
-                method: "POST"
-            });
-        return response.data;
-    } catch (error) {
-        throw error;
-    };
+export const signIn = async (user: IUserSignIn): Promise<IUser | never> => {
+  try {
+    const response = await request.requestApi({
+      route: `user/signin`,
+      body: user,
+      method: 'POST',
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export const signUp = async (user: IUserCreate): Promise<IUser|never> => {
-    try {
-        const response = await request
-            .requestApi({
-                route: `user/signup`,
-                body: user,
-                method: "POST"
-            });    
-        return response.data;
-    } catch (error) {
-        throw error;
-    };
+export const signUp = async (user: IUserCreate): Promise<IUser | never> => {
+  try {
+    const response = await request.requestApi({
+      route: `user/signup`,
+      body: user,
+      method: 'POST',
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const getUsers = async (
-    searchValue: string, 
-    skip: number, 
-    limit: number
-): Promise<IPeopleItem[]|never> => {
-    try {
-        const body = {
-            searchValue,
-            skip,
-            limit,
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  searchValue: string,
+  skip: number,
+  limit: number,
+): Promise<IPeopleItem[] | never> => {
+  try {
+    const body = {
+      searchValue,
+      skip,
+      limit,
     };
+
+    const response = await request.requestApi({
+      route: `user`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
-export const getUserById = async (
-    userId: string
-): Promise<IUser|never> => {
-    try {
-        const body = {
-            userId
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/by_id`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+export const getUserById = async (userId: string): Promise<IUser | never> => {
+  try {
+    const body = {
+      userId,
     };
+
+    const response = await request.requestApi({
+      route: `user/by_id`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const logoutUser = async (
-    userId: string,
-    socketId: string,
-): Promise<IUser|never> => {
-    try {
-        const body = {
-            userId,
-            socketId
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/logout`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  userId: string,
+  socketId: string,
+): Promise<IUser | never> => {
+  try {
+    const body = {
+      userId,
+      socketId,
     };
+
+    const response = await request.requestApi({
+      route: `user/logout`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const changeAvatar = async (
-    body: any
-): Promise<{ 
-    message: string; 
-    avatar: string; 
-}|never> => {
-    try {
-        const response = await request
-            .requestApi({
-                route: `user/avatar`,
-                body,
-                method: "PUT",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
-    };
+  body: any,
+): Promise<
+  | {
+      message: string;
+      avatar: string;
+    }
+  | never
+> => {
+  try {
+    const response = await request.requestApi({
+      route: `user/avatar`,
+      body,
+      method: 'PUT',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const updateProfileInfo = async (
-    userId: string,
-    property: string,
-    newValue: string,
-): Promise<{
-    message: string;
-    newValue: string;
-}|never> => {
-    try {
-
-        const body = {
-            userId,
-            property,
-            newValue
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/profile`,
-                body,
-                method: "PUT",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  userId: string,
+  property: string,
+  newValue: string,
+): Promise<
+  | {
+      message: string;
+      newValue: string;
+    }
+  | never
+> => {
+  try {
+    const body = {
+      userId,
+      property,
+      newValue,
     };
+
+    const response = await request.requestApi({
+      route: `user/profile`,
+      body,
+      method: 'PUT',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const changeEmail = async (
-    userId: string,
-    newEmail: string,
-): Promise<{
-    message: string;
-    value?: string | null;
-}|never> => {
-    try {
-
-        const body = {
-            userId,
-            newEmail
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/change_email`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  userId: string,
+  newEmail: string,
+): Promise<
+  | {
+      message: string;
+      value?: string | null;
+    }
+  | never
+> => {
+  try {
+    const body = {
+      userId,
+      newEmail,
     };
+
+    const response = await request.requestApi({
+      route: `user/change_email`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const validateExistsEmailChange = async (
-    userId: string,
-): Promise<{
-    message: string;
-    value?: string | null;
-}|never> => {
-    try {
-
-        const body = {
-            userId,
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/validate_exists_email_change`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  userId: string,
+): Promise<
+  | {
+      message: string;
+      value?: string | null;
+    }
+  | never
+> => {
+  try {
+    const body = {
+      userId,
     };
+
+    const response = await request.requestApi({
+      route: `user/validate_exists_email_change`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const cancelEmailPendingOfChange = async (
-    userId: string,
-): Promise<{
-    message: string;
-}|never> => {
-    try {
-
-        const body = {
-            userId,
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/cancel_email_pending_change`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  userId: string,
+): Promise<
+  | {
+      message: string;
+    }
+  | never
+> => {
+  try {
+    const body = {
+      userId,
     };
+
+    const response = await request.requestApi({
+      route: `user/cancel_email_pending_change`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const validatePassword = async (
-    userId: string,
-    password: string
-): Promise<{
-    message: string;
-    value: boolean
-}|never> => {
-    try {
-
-        const body = {
-            userId,
-            password
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/validate_password`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  userId: string,
+  password: string,
+): Promise<
+  | {
+      message: string;
+      value: boolean;
+    }
+  | never
+> => {
+  try {
+    const body = {
+      userId,
+      password,
     };
+
+    const response = await request.requestApi({
+      route: `user/validate_password`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const updatePassword = async (
-    userId: string,
-    newPassword: string
-): Promise<{
-    message: string;
-    value: boolean
-}|never> => {
-    try {
-
-        const body = {
-            userId,
-            newPassword
-        };
-
-        const response = await request
-            .requestApi({
-                route: `user/update_password`,
-                body,
-                method: "POST",
-                authorization: true
-            });    
-            
-        return response.data;
-    } catch (error) {
-        throw error;
+  userId: string,
+  newPassword: string,
+): Promise<
+  | {
+      message: string;
+      value: boolean;
+    }
+  | never
+> => {
+  try {
+    const body = {
+      userId,
+      newPassword,
     };
+
+    const response = await request.requestApi({
+      route: `user/update_password`,
+      body,
+      method: 'POST',
+      authorization: true,
+    });
+
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
 };
